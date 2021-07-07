6b1202cb9f0a66a2119c4b20c60df074499dc524
HP <HP@DESKTOP-GAD155C>
2021-07-07 12:28:53 +0530

    first commit

---
+++ com-fraud-consumer-api/src/test/java/com/fraud/consumer/api/ComFraudConsumerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fraud.consumer.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComFraudConsumerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
